64398e47800da68d47e994327ecbc41ba6ce0bc8
2545666 <2545345054@qq.com>
2026-03-16 15:22:17 +0800

    3

--- Week 3/On-Class/4.cpp
+++ Week 3/On-Class/4.cpp
@@ -3,19 +3,22 @@ using namespace std;
 int main () {
 int a;
 int b;
+int sum;
+sum=0;
 cin>>a>>b;
 if (a<b)
-cout<<"偶数："<<endl;
+cout<<"偶数之和："<<endl;
 else
 cout<<"error"<<endl;
 int i=a;
 while (i<=b)
-{
- if (i%2==0)
  {
- cout<<i<<endl;
- i++;
- }
- return 0;
+ if (i%2==0)
+  {
+ sum+=i;
+  } 
+i++;
 }
+ cout<<sum<<endl;
+ return 0;
 }
